312d88559c6f18d9a27c0aa25331669bf2e8256c
Alise Bruevich <abruevic@ucsd.edu>
2021-02-24 22:20:52 -0800

    finished lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = "R8lApdK7EoqGnK4hQbUbWGrNR16s7iLm";
+
+  var map = L.mapquest.map('map',{
+    center:[32.87855993585355, -117.23583508689596],
+    layers: L.mapquest.tileLayer('map'),
+    zoom:14,
+    zoomControl: false
+  });
+
+  L.marker([32.87855993585355, -117.23583508689596]).addTo(map);
 }
